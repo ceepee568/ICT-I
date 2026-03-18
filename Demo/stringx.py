81b54339106ea349cc8f33b195d6069cf328ad63
unknown <08250265.sce@rub.edu.bt>
2026-03-18 09:52:32 +0530

    stringx

---
+++ Demo/stringx.py
@@ -0,0 +1,13 @@
+print("Hello")
+print('Hello')
+multiplelines='''this is a string
+that spans multiple lines.'''
+print(multiplelines)
+a=1
+b=2
+c=a+b
+print(type(c))
+c=str(c)
+print(type(c))
+print(c)
+
