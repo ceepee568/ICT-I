223837936e366132e4c9a68ca747b33de221b5a3
unknown <08250265.sce@rub.edu.bt>
2026-03-20 16:25:33 +0530

    activity

---
+++ Demo/stdactivity.py
@@ -0,0 +1,18 @@
+Name=(input("Please enter your name: "))
+a=float(input("Please enter the first number:"))
+b=float(input("Please enter the second number: "))
+addition_result=a+b
+subtraction_result=a-b
+multiplication_result=a*b
+division_result=a/b
+exponentiation_result=a**b
+modulus_result=a%b
+print("Dear ", Name)
+print("The numbers below are the solutions of the operations")
+print("Sum: ",addition_result)
+print("Difference: ",subtraction_result)
+print("Product:",multiplication_result)
+print("Quotient:",division_result)
+print("Power",exponentiation_result) 
+print("Remainder",modulus_result)
+
