a08e270d930a6b06d7d926ba2c9554c747752cfa
unknown <08250265.sce@rub.edu.bt>
2026-03-20 15:31:37 +0530

    area of rectangle

--- Demo/areaofrectangle.py
+++ Demo/areaofrectangle.py
@@ -2,3 +2,4 @@ L=float(input("enter the length of the rectangle: "));
 B=float(input("enter the bredth of the rectangle: "));
 area=L*B
 print("the area of the rectangle is: ", area);
+print("the area of the rectangle is: "+ str(area))
